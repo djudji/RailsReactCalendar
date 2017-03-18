11175493993e2aa9bfc9a504307966181667c018
Zlatko Alomerovic <zlatko.alomerovic@gmail.com>
2017-03-18 15:18:30 +0100

    Create appointments with AJAX and Rails

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,4 +1,22 @@
 class AppointmentsController < ApplicationController
-  def index
+  before_action :set_appointments, only: [:index, :create]
+
+  def index; end
+
+  def create
+    @appointment = Appointment.new(appointment_params)
+    respond_to do |format|
+      format.js { render :index unless @appointment.save }
+    end
+  end
+
+  private
+
+  def set_appointments
+    @appointments = Appointment.order(appt_time: :asc)
+  end
+
+  def appointment_params
+    params.require(:appointment).permit(:title, :appt_time)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root 'appointments#index'
+  resource :appointments
 end
