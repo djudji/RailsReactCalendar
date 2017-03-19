c1373ad2962d20333adc44c678ab5e4e7489624b
Zlatko Alomerovic <zlatko.alomerovic@gmail.com>
2017-03-19 14:58:45 +0100

    Get initial state of form input fields

--- app/assets/javascripts/components/appointment_form.jsx
+++ app/assets/javascripts/components/appointment_form.jsx
@@ -1,11 +1,13 @@
 var AppointmentForm = React.createClass({
-  render: function(){
+  render: function() {
     return (
       <div id="new-appt">
         <h5>Make a new appointment</h5>
         <form className="form-inline">
-          <input type="text" name="title" placeholder="Appointment Title" className="form-control" />
-          <input type="text" name="appt_time" placeholder="Date and Time" className="form-control" />
+          <input name="title" className="form-control"
+                 value={this.props.input_title} />
+          <input name="appt_time" className="form-control"
+                 value={this.props.input_appt_time} />
           <input type="submit" value="Make Appointment" className="btn btn-primary" />
         </form>
       </div>
--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -1,10 +1,18 @@
 var Appointments = React.createClass({
-  render: function(){
-    return (
+  getInitialState: function() {
+    return {
+      appointments: this.props.appointments,
+      title: "Team meeting",
+      appt_time: "Tomorrow at 9am"
+    }
+  },
+
+  render: function() {
+    return(
       <div>
-        <AppointmentForm />
-        <AppointmentsList appointments={this.props.appointments} />
+        <AppointmentForm input_title={this.state.title} input_appt_time={this.state.appt_time} />
+        <AppointmentsList appointments={this.state.appointments} />
       </div>
-    );
+    )
   }
 });
