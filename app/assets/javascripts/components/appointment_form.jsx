b6cc5d082646c3fbce0b5862a3b63cd767402573
Zlatko Alomerovic <zlatko.alomerovic@gmail.com>
2017-03-28 15:05:33 +0200

    Switch the form to use ES6

--- app/assets/javascripts/components/appointment_form.jsx
+++ app/assets/javascripts/components/appointment_form.jsx
@@ -1,39 +1,39 @@
-var AppointmentForm = React.createClass({
-  handleChange: function(e){
-    var name = e.target.name;
+class AppointmentForm extends React.Component {
+  handleChange(e) {
+    const name = e.target.name;
     obj = {};
     obj[name] = e.target.value;
     this.props.onUserInput(obj);
-  },
+  }
 
-  handleSubmit: function(e){
+  handleSubmit(e) {
     e.preventDefault();
     this.props.onFormSubmit();
-  },
+  }
 
-  setApptTime: function(e){
-    var name = "appt_time";
-    var obj = {};
+  setApptTime(e) {
+    const name = "appt_time";
+    const obj = {};
     if(obj[name] = e.toDate()){
       this.props.onUserInput(obj);
     }
-  },
+  }
 
-  render: function() {
-    var inputProps = {
+  render() {
+    const inputProps = {
       name: "appt_time"
     };
     return (
       <div id="new-appt">
         <h5>Make a new appointment</h5>
-        <form className="form" onSubmit = { this.handleSubmit }>
+        <form className="form" onSubmit = { this.handleSubmit.bind(this) }>
           <input name="title" className="form-control"
-                 value={this.props.input_title} onChange={this.handleChange} />
+                 value={this.props.input_title} onChange={ this.handleChange.bind(this) } />
           <Label label="Choose date and time" />
-          <Datetime input={false} open={true} inputProps={inputProps} value={this.props.appt_time} onChange={this.setApptTime} />
+          <Datetime input={false} open={true} inputProps={inputProps} value={this.props.appt_time} onChange={ this.setApptTime.bind(this) } />
           <input type="submit" value="Make Appointment" className="btn btn-primary" />
         </form>
       </div>
     )
   }
-});
+}
