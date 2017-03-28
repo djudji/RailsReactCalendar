51e1ae5c3789f1635b38a6aaaea344ad5e4efdaa
Zlatko Alomerovic <zlatko.alomerovic@gmail.com>
2017-03-28 15:19:50 +0200

    Switch appointments to use ES6

---
+++ app/assets/javascripts/components/appointments.es6.jsx
@@ -0,0 +1,43 @@
+class Appointments extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      appointments: this.props.appointments,
+      title: "Team meeting",
+      appt_time: "Tomorrow at 9am"
+    }
+  }
+
+  handleUserInput(obj) {
+    this.setState(obj);
+  }
+
+  handleFormSubmit() {
+    let appointment = {title: this.state.title, appt_time: this.state.appt_time};
+    $.post("/appointments",
+           {appointment: appointment}).done(function(data){
+            this.addNewAppointment(data);
+           }.bind(this));
+  }
+
+  addNewAppointment(appointment) {
+    let appointments = React.addons.update(this.state.appointments, { $push: [appointment] });
+    this.setState({
+      appointments: appointments.sort(function(a,b){
+        return new Date(a.appt_time) - new Date(b.appt_time);
+      })
+    });
+  }
+
+  render() {
+    return(
+      <div>
+        <AppointmentForm input_title = { this.state.title } appt_time = { this.state.appt_time }
+                         onUserInput = { this.handleUserInput.bind(this) }
+                         onFormSubmit = { this.handleFormSubmit.bind(this) } />
+        <AppointmentsList appointments = { this.state.appointments } />
+      </div>
+    )
+  }
+}
